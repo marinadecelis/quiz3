3e4398bfd7cff11e0474b238b59fdbe477c222be
marinadecelis <marina88informatica@gmail.com>
2015-07-30 22:36:46 +0200

    Validacionb
    mod8

--- models/quiz.js
+++ models/quiz.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
 	{	pregunta:{
 			type: DataTypes.STRING,
 			validate: { notEmpty: {msg: "->	Falta Pregunta"}}
-	},
+		},
 	respuesta:{
 		type: DataTypes.STRING,
 		validate: { notEmpty: {msg:	"-> Falta Respuesta"}}
